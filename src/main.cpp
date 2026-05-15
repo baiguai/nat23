089818f6392e97c5f5e80dcf00b4eb2f2a9b762d
baiguai <baiguai.junk@gmail.com>
2026-05-15 16:47:01 -0700

    Added more validation.

--- src/main.cpp
+++ src/main.cpp
@@ -169,7 +169,7 @@ namespace Nat23
             help_txt += "4*1d8";
             help_txt += "\n\n\n";
             help_txt += "Best n of n --  ";
-            help_txt += "If present, the total will only take the best n values out of the total rolls specified in the basic roll.\n";
+            help_txt += "If present, the total will only take the best n values out of the total rolls specified in the basic roll (must be less than the number of dice).\n";
             help_txt += "(<best of count>)<basic roll>\n\n";
             help_txt += "Example:\n";
             help_txt += "(2)4d6\n";
@@ -222,12 +222,17 @@ namespace Nat23
             int dice_sides { 1 };
             int advantage { 0 };
             int penalty { 0 };
+            const int safety_limit { 500 };
 
             last_roll = rollstring;
 
             try
             {
                 times = std::stoi(getSplit(rollstring, "*", 0, "1"));
+                if (times > safety_limit)
+                {
+                    times = safety_limit;
+                }
                 rollstring = getSplit(rollstring, "*", 1, rollstring);
 
                 if (getSplit(rollstring, "(", 0, "NA") != "NA" && getSplit(rollstring, ")", 0, "NA") != "NA")
@@ -241,29 +246,37 @@ namespace Nat23
                 if (getSplit(rollstring, "+", 0, "NA") != "NA")
                 {
                     advantage = std::stoi(getSplit(rollstring, "+", 1, "0"));
+                    if (advantage > safety_limit)
+                    {
+                        advantage = safety_limit;
+                    }
                     rollstring = getSplit(rollstring, "+", 0, rollstring);
                 }
 
                 if (getSplit(rollstring, "-", 0, "NA") != "NA")
                 {
                     penalty = std::stoi(getSplit(rollstring, "-", 1, "0"));
+                    if (penalty > safety_limit)
+                    {
+                        penalty = safety_limit;
+                    }
                     rollstring = getSplit(rollstring, "-", 0, rollstring);
                 }
 
                 dice_num = std::stoi(getSplit(rollstring, "d", 0, "0"));
                 dice_sides = std::stoi(getSplit(rollstring, "d", 1, "0"));
 
-                if (dice_num < 1 || dice_sides < 2)
+                if (dice_num < 1 || dice_sides < 2 || best_of >= dice_num)
                 {
                     last_roll = "";
-                    showHelp();
+                    showHelpRolls();
                     return;
                 }
             }
             catch (const std::invalid_argument& e)
             {
                 last_roll = "";
-                showHelp();
+                showHelpRolls();
                 return;
             }
 
